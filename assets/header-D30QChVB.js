a5b4f3709362ff8cb7309e64aab15e63d674ca67
sokol80 <85589625+sokol80@users.noreply.github.com>
2025-06-11 10:11:34 +0300

    Пятый

---
+++ assets/header-D30QChVB.js
@@ -0,0 +1,29 @@
+const n=`<header class="header">\r
+  <div class="container">\r
+    <div class="header-content">\r
+      <div class="logo">\r
+        <a href="/">\r
+          <!-- <img src="/src/assets/logo.svg" alt="Логотип" /> -->\r
+        </a>\r
+      </div>\r
+\r
+      <nav class="main-nav">\r
+        <ul>\r
+          <li><a href="/my-vite-site-2/" class="nav-link">Главная</a></li>\r
+          <li><a href="/my-vite-site-2/about/" class="nav-link">О нас</a></li>\r
+          <li>\r
+            <a href="/my-vite-site-2/services/" class="nav-link">Услуги</a>\r
+          </li>\r
+          <li>\r
+            <a href="/my-vite-site-2/portfolio/" class="nav-link">Портфолио</a>\r
+          </li>\r
+          <li>\r
+            <a href="/my-vite-site-2/contact/" class="nav-link">Контакты</a>\r
+          </li>\r
+          <li><a href="/my-vite-site-2/blog/" class="nav-link">Блог</a></li>\r
+        </ul>\r
+      </nav>\r
+    </div>\r
+  </div>\r
+</header>\r
+`;export{n as default};
