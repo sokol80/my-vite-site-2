be02e14b1d314e400c0c77b2e185b09f8ef159a8
sokol80 <85589625+sokol80@users.noreply.github.com>
2025-06-11 09:55:47 +0300

    Третий

---
+++ assets/header-ETIiSoUu.js
@@ -0,0 +1,23 @@
+const a=`<header class="header">\r
+  <div class="container">\r
+    <div class="header-content">\r
+      <div class="logo">\r
+        <a href="/">\r
+          <!-- <img src="/src/assets/logo.svg" alt="Логотип" /> -->\r
+        </a>\r
+      </div>\r
+\r
+      <nav class="main-nav">\r
+        <ul>\r
+          <li><a href="./" class="nav-link">Главная</a></li>\r
+          <li><a href="./about/" class="nav-link">О нас</a></li>\r
+          <li><a href="./services/" class="nav-link">Услуги</a></li>\r
+          <li><a href="./portfolio/" class="nav-link">Портфолио</a></li>\r
+          <li><a href="./contact/" class="nav-link">Контакты</a></li>\r
+          <li><a href="./blog/" class="nav-link">Блог</a></li>\r
+        </ul>\r
+      </nav>\r
+    </div>\r
+  </div>\r
+</header>\r
+`;export{a as default};
